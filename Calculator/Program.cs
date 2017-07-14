307250260e7aa5ebf1ffabc7654321216ffb849f
ollieOsh <sosinusi@gmail.com>
2017-07-14 15:32:41 -0500

    horse hole

---
+++ Calculator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Calculator
+{ 
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
